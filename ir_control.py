ef279842f17fa290d3ea1d8d2593fcfaf245ff0b
yuto0727 <wy0727.watyu20@gmail.com>
2022-11-30 10:06:14 +0900

    irrpを用いてIR制御

--- ir_control.py
+++ ir_control.py
@@ -1,28 +1,21 @@
-from time import sleep
-import RPi.GPIO as GPIO
-import pigpio, os, json
+import irrp as ir
 
-IR_RX_PIN = 18
-GLITCH = 100
-PRE_MS = 200
-POST_MS = 15
-FREQ = 38.0
-SHORT = 10
-TOLERANCE = 15
+ir.GPIO = 18
 
-POST_US = POST_MS * 1000
-PRE_US = PRE_MS * 1000
-TOLER_MIN = (100 - TOLERANCE) / 100.0
-TOLER_MAX = (100 + TOLERANCE) / 100.0
+ir.GLITCH = 100
+ir.PRE_MS = 200
+ir.POST_MS = 15
+ir.VERBOSE = False
+ir.SHORT = 10
+ir.TOLERANCE = 15
 
-last_tick = 0
-in_code = False
-code = []
-fetching_code = False
+ir.POST_US = POST_MS * 1000
+ir.PRE_US = PRE_MS * 1000
+ir.TOLER_MIN = (100 - TOLERANCE) / 100.0
+ir.TOLER_MAX = (100 + TOLERANCE) / 100.0
+
 
 def main():
-    global fetching_code, code
-
     pi = pigpio.pi()
 
     if not pi.connected:
@@ -30,22 +23,22 @@ def main():
 
     with open('codes_for_control') as f:
         key_config = json.load(f)
-        pi.set_mode(IR_RX_PIN, pigpio.INPUT)
-        pi.set_glitch_filter(IR_RX_PIN, GLITCH)
+        pi.set_mode(ir.GPIO, pigpio.INPUT)
+        pi.set_glitch_filter(ir.GPIO, ir.GLITCH)
 
-        cb = pi.callback(IR_RX_PIN, pigpio.EITHER_EDGE, cbf)
+        cb = pi.callback(ir.GPIO, pigpio.EITHER_EDGE, ir.cbf)
 
         try:
             while True:
-                code = []
-                fetching_code = True
-                while fetching_code:
+                ir.code = []
+                ir.fetching_code = True
+                while ir.fetching_code:
                     time.sleep(0.1)
 
                 time.sleep(0.5)
 
                 for key, val in key_config.items():
-                    if compare(val, code[:]):
+                    if ir.compare(val, ir.code[:]):
                         key_name = key
 
                 if key_name == "power":
@@ -63,87 +56,3 @@ def main():
             pass
         finally:
             pi.stop()
-
-
-
-def normalize(c):
-    entries = len(c)
-    p = [0]*entries  # Set all entries not processed.
-    for i in range(entries):
-        if not p[i]:  # Not processed?
-            v = c[i]
-            tot = v
-            similar = 1.0
-
-            # Find all pulses with similar lengths to the start pulse.
-            for j in range(i+2, entries, 2):
-                if not p[j]:  # Unprocessed.
-                    if (c[j]*TOLER_MIN) < v < (c[j]*TOLER_MAX):  # Similar.
-                        tot = tot + c[j]
-                        similar += 1.0
-
-            # Calculate the average pulse length.
-            newv = round(tot / similar, 2)
-            c[i] = newv
-
-            # Set all similar pulses to the average value.
-            for j in range(i+2, entries, 2):
-                if not p[j]:  # Unprocessed.
-                    if (c[j]*TOLER_MIN) < v < (c[j]*TOLER_MAX):  # Similar.
-                        c[j] = newv
-                        p[j] = 1
-
-
-def compare(p1, p2):
-    if len(p1) != len(p2):
-        return False
-
-    for i in range(len(p1)):
-        v = p1[i] / p2[i]
-        if (v < TOLER_MIN) or (v > TOLER_MAX):
-            return False
-
-    for i in range(len(p1)):
-        p1[i] = int(round((p1[i]+p2[i])/2.0))
-
-    return True
-
-
-def end_of_code():
-    global code, fetching_code
-    if len(code) > SHORT:
-        normalize(code)
-        fetching_code = False
-    else:
-        code = []
-        print("Short code, probably a repeat, try again")
-
-
-def cbf(gpio, level, tick):
-    global last_tick, in_code, code, fetching_code
-
-    if level != pigpio.TIMEOUT:
-        edge = pigpio.tickDiff(last_tick, tick)
-        last_tick = tick
-
-        if fetching_code:
-            if (edge > PRE_US) and (not in_code):  # Start of a code.
-                in_code = True
-                pi.set_watchdog(IR_RX_PIN, POST_MS)  # Start watchdog.
-
-            elif (edge > POST_US) and in_code:  # End of a code.
-                in_code = False
-                pi.set_watchdog(IR_RX_PIN, 0)  # Cancel watchdog.
-                end_of_code()
-
-            elif in_code:
-                code.append(edge)
-
-    else:
-        pi.set_watchdog(IR_RX_PIN, 0)  # Cancel watchdog.
-        if in_code:
-            in_code = False
-            end_of_code()
-
-if __name__ == "__main__":
-    main()
